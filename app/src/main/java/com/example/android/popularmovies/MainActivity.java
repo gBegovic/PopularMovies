2dfe31b877d6d9a52f33170de080f9fd7b0c11ef
Goran Begovic <gxb5080@g.rit.edu>
2017-05-26 00:50:57 +0200

    DB functionality added

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,24 +2,36 @@ package com.example.android.popularmovies;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.example.android.popularmovies.data.MoviesContract;
 import com.example.android.popularmovies.utilities.JsonUtils;
 import com.example.android.popularmovies.utilities.NetworkUtils;
 import org.json.JSONException;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_EN_TITLE;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_MOVIE_ID;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_POSTER;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.MOVIE_DETAILS_PROJECTION;
 
 /**
  * Main activity class which is started upon application launch, handles the display of movie thumbnails
@@ -34,8 +46,61 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
     private TextView errorMessage;
     private int queryType = 1;
     private static WeakReference<Context> contextReference;
-    private static final int ID_MOVIES_LOADER = 20;
+    private static final int ID_MOVIES_LOADER = 21;
+    private static final int ID_DB_MOVIES_LOADER = 22;
     private static final String SEARCH_QUERY_URL_EXTRA = "query";
+
+    private LoaderManager.LoaderCallbacks<Cursor> dbLoader
+            = new LoaderManager.LoaderCallbacks<Cursor>() {
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+            switch (id){
+
+                case ID_DB_MOVIES_LOADER:
+                    return new CursorLoader(MainActivity.this,
+                            MoviesContract.MoviesEntry.CONTENT_URI,
+                            MOVIE_DETAILS_PROJECTION,
+                            null,
+                            null,
+                            null);
+
+                default:
+                    throw new RuntimeException("Loader Not Implemented: " + id);
+            }
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+            if (!(data != null && data.moveToFirst() && queryType == 3)){
+
+                String[] emptyData = {""};
+                moviesAdapter.setMoviesPosters(emptyData);
+                return;
+            }
+
+            String[] stringData = new String[data.getCount()*3];
+            Log.v("DATA: ", data.getCount()+"  " + stringData.length);
+            for (int i = 0; i < stringData.length; i+=3){
+
+                stringData[i] = data.getString(INDEX_POSTER);
+                stringData[i+1] = data.getString(INDEX_EN_TITLE);
+                stringData[i+2] = data.getInt(INDEX_MOVIE_ID) +"";
+                Log.v("DATA: ", data.getString(INDEX_POSTER) + "  " + data.getString(INDEX_EN_TITLE) + "  " + data.getInt(INDEX_MOVIE_ID));
+                data.moveToNext();
+            }
+
+            moviesAdapter.setMoviesPosters(stringData);
+            showContent();
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+    };
 
     /**
      * Method that initializes the activity
@@ -56,14 +121,25 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
         recyclerView.setAdapter(moviesAdapter);
 
         if(savedInstanceState != null){
-
+            Log.v("ON CREATE", "Saved instance working " + queryType);
             queryType = savedInstanceState.getInt(SEARCH_QUERY_URL_EXTRA);
-            getSupportLoaderManager().restartLoader(ID_MOVIES_LOADER, savedInstanceState, this);
+            if (queryType == 3){
+                getSupportLoaderManager().restartLoader(ID_DB_MOVIES_LOADER, null, dbLoader);
+            }
+            else {
+                getSupportLoaderManager().restartLoader(ID_MOVIES_LOADER, savedInstanceState, this);
+            }
         }
         else {
+            Log.v("ON CREATE", "New Bundle " + queryType);
             Bundle bundle = new Bundle();
             bundle.putInt(SEARCH_QUERY_URL_EXTRA, queryType);
-            getSupportLoaderManager().initLoader(ID_MOVIES_LOADER, bundle, this);
+            if (queryType == 3){
+                getSupportLoaderManager().initLoader(ID_DB_MOVIES_LOADER, null, dbLoader);
+            }
+            else {
+                getSupportLoaderManager().initLoader(ID_MOVIES_LOADER, bundle, this);
+            }
         }
     }
 
@@ -92,6 +168,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
 
         Intent intent = new Intent(this, MovieDetailsActivity.class);
         intent.putExtra(Intent.EXTRA_TEXT, movieId);
+        intent.putExtra(getString(R.string.extra_query_type), queryType);
         startActivity(intent);
     }
 
@@ -131,6 +208,12 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
                 queryType = 2;
                 bundle.putInt(SEARCH_QUERY_URL_EXTRA, queryType);
                 getSupportLoaderManager().restartLoader(ID_MOVIES_LOADER, bundle, this);
+                return true;
+
+            case R.id.action_favorites:
+                setTitle(getString(R.string.favorite_movies_label));
+                queryType = 3;
+                getSupportLoaderManager().initLoader(ID_DB_MOVIES_LOADER, null, dbLoader);
                 return true;
 
             default:
--- app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
@@ -1,21 +1,26 @@
 package com.example.android.popularmovies;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
-
+import com.example.android.popularmovies.data.MoviesContract;
 import com.example.android.popularmovies.utilities.ArrayUtils;
 import com.example.android.popularmovies.utilities.JsonUtils;
 import com.example.android.popularmovies.utilities.NetworkUtils;
@@ -26,6 +31,16 @@ import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_EN_TITLE;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_LANGUAGE;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_MOVIE_ID;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_ORIGINAL_TITLE;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_PLOT;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_POSTER;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_RATING;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.INDEX_RELEASE_DATE;
+import static com.example.android.popularmovies.data.MoviesContract.MoviesEntry.MOVIE_DETAILS_PROJECTION;
+
 /**
  * Activity class that handles the movie details screen invoked when the user clicks on a movie thumbnail
  * @author Goran Begovic
@@ -34,12 +49,79 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
 
     private TextView originalTitle, title, plot, rating, releaseDate, errorMessage;
     private ImageView poster;
+    private CheckBox favoriteButton;
     private ProgressBar progressBar;
     private String idExtra = "";
     private ScrollView scrollView;
     private LinearLayout linearLayout;
+    private int queryType;
+    private ContentValues contentValues = new ContentValues();
     private static final int ID_MOVIES_LOADER = 21;
+    private static final int ID_DB_MOVIES_LOADER = 22;
     private static final String SEARCH_QUERY_URL_EXTRA = "query";
+    private static final String SEARCH_QUERY_TYPE_EXTRA = "queryType";
+    private static final String SELECTION_BY_MOVIE_ID = MoviesContract.MoviesEntry.COLUMN_MOVIE_ID + " = ? ";
+
+    private LoaderManager.LoaderCallbacks<Cursor> dbLoader
+            = new LoaderManager.LoaderCallbacks<Cursor>() {
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+            switch (id){
+
+                case ID_DB_MOVIES_LOADER:
+                    return new CursorLoader(MovieDetailsActivity.this,
+                            MoviesContract.MoviesEntry.buildMovieUriWithMovieId(idExtra),
+                            MOVIE_DETAILS_PROJECTION,
+                            null,
+                            null,
+                            null);
+
+                default:
+                    throw new RuntimeException("Loader Not Implemented: " + id);
+            }
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+            if (!(data != null && data.moveToFirst())){
+
+                showErrorMessage();
+                return;
+            }
+            Log.v("ON LOAD", data.getString(INDEX_ORIGINAL_TITLE));
+            originalTitle.setText(data.getString(INDEX_ORIGINAL_TITLE));
+
+            if (!data.getString(INDEX_LANGUAGE).equals(getString(R.string.language_en))) {
+                title.setText(getString(R.string.title, data.getString(INDEX_EN_TITLE)));
+                title.setVisibility(View.VISIBLE);
+            }
+
+            Picasso.with(MovieDetailsActivity.this).load(getString(R.string.movie_poster_base_url_w780) + data.getString(INDEX_POSTER)).into(poster);
+
+            try {
+                releaseDate.setText(getString(R.string.details_release_date,
+                        new SimpleDateFormat(getString(R.string.output_date_format))
+                                .format(new SimpleDateFormat(getString(R.string.input_date_format))
+                                        .parse(data.getString(INDEX_RELEASE_DATE)
+                                        )
+                                )
+                ));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            rating.setText(getString(R.string.details_rating, data.getString(INDEX_RATING)));
+            plot.setText(getString(R.string.details_plot, data.getString(INDEX_PLOT)));
+            showContent();
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+    };
 
     /**
      * Method that initializes the activity
@@ -58,22 +140,54 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
         rating = (TextView) findViewById(R.id.tv_details_rating);
         releaseDate = (TextView) findViewById(R.id.tv_details_release_date);
         poster = (ImageView) findViewById(R.id.iv_details_poster);
+        favoriteButton = (CheckBox) findViewById(R.id.favorite_button);
         errorMessage = (TextView) findViewById(R.id.tv_details_error_message);
         progressBar = (ProgressBar) findViewById(R.id.pb_details_loader);
+
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(favoriteButton.isChecked()){
+                    getContentResolver().insert(MoviesContract.MoviesEntry.CONTENT_URI, contentValues);
+                }
+                else{
+                    String[] selectionArgs = {contentValues.getAsString(MOVIE_DETAILS_PROJECTION[0])};
+                    getContentResolver().delete(MoviesContract.MoviesEntry.CONTENT_URI, SELECTION_BY_MOVIE_ID, selectionArgs);
+                }
+            }
+        });
+
         Intent intent = getIntent();
-        if (intent.hasExtra(Intent.EXTRA_TEXT))
+        if (intent.hasExtra(Intent.EXTRA_TEXT) && intent.hasExtra(getString(R.string.extra_query_type))) {
+
             idExtra = intent.getStringExtra(Intent.EXTRA_TEXT);
+            queryType = intent.getIntExtra(getString(R.string.extra_query_type), 0);
+        }
 
         if(savedInstanceState != null){
 
             idExtra = savedInstanceState.getString(SEARCH_QUERY_URL_EXTRA);
-            getSupportLoaderManager().restartLoader(ID_MOVIES_LOADER, savedInstanceState, this);
+            queryType = savedInstanceState.getInt(SEARCH_QUERY_TYPE_EXTRA);
+            Log.v("ON CREATE", "Saved instance working " + queryType);
+            if (queryType != 3){
+                getSupportLoaderManager().restartLoader(ID_MOVIES_LOADER, savedInstanceState, this);
+            }
+            else {
+                getSupportLoaderManager().initLoader(ID_DB_MOVIES_LOADER, null, dbLoader);
+            }
         }
         else {
+            Log.v("ON CREATE", "New Bundle " + queryType);
             Bundle bundle = new Bundle();
             bundle.putString(SEARCH_QUERY_URL_EXTRA, idExtra);
-            getSupportLoaderManager().initLoader(ID_MOVIES_LOADER, bundle, this);
+            if (queryType != 3){
+                getSupportLoaderManager().initLoader(ID_MOVIES_LOADER, bundle, this);
+            }
+            else {
+                getSupportLoaderManager().initLoader(ID_DB_MOVIES_LOADER, null, dbLoader);
+            }
         }
+
     }
 
     @Override
@@ -81,6 +195,7 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
         super.onSaveInstanceState(outState);
 
         outState.putString(SEARCH_QUERY_URL_EXTRA, idExtra);
+        outState.putInt(SEARCH_QUERY_TYPE_EXTRA, queryType);
     }
 
     /**
@@ -162,16 +277,32 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
 
         if (movieDetails != null) {
 
-            originalTitle.setText(movieDetails[0]);
-
-            if(!movieDetails[6].equals(getString(R.string.language_en))) {
-                title.setText(getString(R.string.title, movieDetails[1]));
-                title.setVisibility(View.VISIBLE);
+            if(queryType == 3){
+                return;
             }
-            Picasso.with(MovieDetailsActivity.this).load(getString(R.string.movie_poster_base_url_w780) + movieDetails[2]).into(poster);
+            else {
+                originalTitle.setText(movieDetails[0]);
 
-            plot.append(movieDetails[3]);
-            rating.append(movieDetails[4]);
+                if (!movieDetails[6].equals(getString(R.string.language_en))) {
+                    title.setText(getString(R.string.title, movieDetails[1]));
+                    title.setVisibility(View.VISIBLE);
+                }
+                Picasso.with(MovieDetailsActivity.this).load(getString(R.string.movie_poster_base_url_w780) + movieDetails[2]).into(poster);
+
+                plot.setText(getString(R.string.details_plot, movieDetails[3]));
+                rating.setText(getString(R.string.details_rating, movieDetails[4]));
+
+                if (linearLayout.getChildCount() > 6) {
+
+                    linearLayout.removeViewsInLayout(6, linearLayout.getChildCount() - 6);
+                }
+
+                try {
+                    releaseDate.setText(getString(R.string.details_release_date, new SimpleDateFormat(getString(R.string.output_date_format)).format(new SimpleDateFormat(getString(R.string.input_date_format)).parse(movieDetails[5]))));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
 
             for(int i = 8 + (Integer.parseInt(movieDetails[7]) * 2); i < movieDetails.length; i+=2){
 
@@ -210,14 +341,16 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
                 textView.append(movieDetails[i+1]+"\n");
             }
 
-            try {
-                releaseDate.append(new SimpleDateFormat(getString(R.string.output_date_format)).format(new SimpleDateFormat(getString(R.string.input_date_format)).parse(movieDetails[5])));
-            }
-            catch (ParseException e) {
-                e.printStackTrace();
-            }
-
             showContent();
+
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_MOVIE_ID], idExtra);
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_ORIGINAL_TITLE], movieDetails[0]);
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_EN_TITLE], movieDetails[1]);
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_POSTER], movieDetails[2]);
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_RELEASE_DATE], movieDetails[5]);
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_RATING], movieDetails[4]);
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_PLOT], movieDetails[3]);
+            contentValues.put(MOVIE_DETAILS_PROJECTION[INDEX_LANGUAGE], movieDetails[6]);
         }
 
         else {
